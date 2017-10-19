19946658acbd5c96d447e344e243ce01a2d2343b
Лейкин Андрей Романович <Leykin.AR@kamaz.org>
2017-10-19 10:09:00 +0300

    add style

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 // Функция js
-
+/*
 $(function(){
 	$('.js-button').click(function() {
 
@@ -59,6 +59,64 @@ $(function(){
 
 	});
 
+	function randomInteger(min, max) {
+		var rand = min + Math.random() * (max + 1 - min);
+		rand = Math.floor(rand);
+		return rand;
+	}
+});
+*/
+
+
+// 2 попытка
+
+$(function(){
+	$('.js-button').click(function() {
+
+		var display = $('.result'),
+			item = $('.item'),
+			startChild = $('.item').children();
+
+			startChild.removeClass('select');
+			display.html(' ');
+
+		for(v=0; v<=item.length-1; v++) {
+            var child = item.eq(v).children();
+            switch(v) {
+              case 0: 
+                randomChild(3, child);
+                break;
+              case 1: 
+                randomChild(1, child);
+                break;
+              case 2: 
+                randomChild(4, child);
+                break;
+            }
+		}
+	});
+
+    function randomChild(count, child) {
+      for(i=0; i<=count; i++) {
+        var index = randomInteger(0, child.length - 1),
+            reqChild = child.eq(index);
+        if(reqChild.hasClass('select')) {
+          i--;
+          continue;
+        } else {
+            reqChild.addClass('select');
+        }
+        var html = reqChild.html();
+        if (i == 0) {
+           $('.result').append('<span class="subtitle">Блок</span><span>' + html + '</span>');
+        } else {
+           $('.result').append('<span>' + html + '</span>');
+        }
+        
+       
+      }
+    }
+  
 	function randomInteger(min, max) {
 		var rand = min + Math.random() * (max + 1 - min);
 		rand = Math.floor(rand);
